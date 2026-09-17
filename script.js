099af2f0f3c5604bb0f7d8e2445ebf53d0a357d3
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-09-17 09:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,14 @@
 
 function palindrome(str){
 
+	for(let i=0; i<str.length; i++){
+
+		if(str[i] !== str[str.length-1-i]){
+			return false
+		}
+	}
+
+	return true
+
 }
 module.exports = palindrome
